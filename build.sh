0ad670c5c1259141cea0d98e81ef5d5c8f83b276
Dmitry Shmelev <avikez@gmail.com>
2017-06-08 20:59:45 +0300

    Add jnlp

--- build.sh
+++ build.sh
@@ -10,10 +10,11 @@ sed -r "$sedStr" $1
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-versions=(0.10.47 4.6.0 5.5.0 6.10.0 6.7.0 7.3.0 7.4.0 7.9.0 8.0.0)
+versions=(0.10 4.x 5.x 6.x 7.x 8.x)
 for version in ${versions[*]}; do
   mkdir -p .build/$version
+  cp jenkins-slave .build/$version
   render Dockerfile.template > .build/$version/Dockerfile
-  docker build -t $DOCKER_REPO:$version .build/$version
-  docker push $DOCKER_REPO:$version
+  docker build -t $DOCKER_REPO:$version-jnlp .build/$version
+  docker push $DOCKER_REPO:$version-jnlp
 done
